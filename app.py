ad0afb7ea5fec38b9a77bad9ac4a071379d34f4c
Alexander7952006 <tretyakalexander2006@gmail.com>
2024-09-28 12:18:31 +0300

    app.py

--- app.py
+++ app.py
@@ -45,27 +45,27 @@ def end(line1, line2, line3):
         line3[0] == line3[1] == line3[2] != [' '] or 
         line1[0] == line2[1] == line3[2] != [' '] or 
         line1[2] == line2[1] == line3[0] != [' '] or
-        line1.count([' '])+line2.count([' '])+line3.count([' ']) ==0):
+        line1.count([' ']) + line2.count([' ']) + line3.count([' ']) == 0):
         flag = 1
     return flag       
-count = 0#счетчик четности хода(четное-первый игрок, нечетное - второй)
+count = 0 #счетчик четности хода(четное-первый игрок, нечетное - второй)
 
 
-while end(line1, line2, line3) !=1:
-    if count%2==0:
+while end(line1, line2, line3) != 1:
+    if count%2 == 0:
         player = 'Первый игрок'
     else:
         player = 'Второй игрок'
     argument = 0 #переменная для проверки того, сделан ли ход игроком
     '''Цикл while работает до тех пор пока игрок не сделает ход'''
-    while argument!=1:
+    while argument != 1:
         '''arg_move - переменная для проверки корректности данных,
     введенных первым игроком'''
         arg_move = 0 
         '''Цикл while работает до тех пор пока игрок не введет подходящие
         значения(целое число от 1 до 3 вкл), это нужно для того чтобы потом
         программа не прекращалась из-за ошибки'''
-        while arg_move!=1:
+        while arg_move != 1:
             move_line = input(f"{player}, введите номер строки, на" + '\n' +
                               'которой хотите поставить знак '
                                '(целое число от 1 до 3): \n')
@@ -82,18 +82,18 @@ while end(line1, line2, line3) !=1:
         '''Если клетка, запрошенная игроком пуста, присвоим
         ей нужное значение'''
         if count%2==0:
-            if line_list[move_line-1][move_column-1]==[' ']:
-                line_list[move_line-1][move_column-1]=['×']
-                argument = 1#ход сделан успешно, присваиваем единицу
-                count+=1 #ход сделан успешно, присваиваем единицу
+            if line_list[move_line-1][move_column-1] == [' ']:
+                line_list[move_line-1][move_column-1] = ['×']
+                argument = 1 #ход сделан успешно, присваиваем единицу
+                count += 1 #ход сделан успешно, присваиваем единицу
             else:
                 print('На этой клетке уже стоит знак, поставьте знак '
                       'на пустую клетку \n')
         else:
-            if line_list[move_line-1][move_column-1]==[' ']:
-                line_list[move_line-1][move_column-1]=['0']
+            if line_list[move_line-1][move_column-1] == [' ']:
+                line_list[move_line-1][move_column-1] = ['0']
                 argument = 1#ход сделан успешно, присваиваем единицу
-                count+=1 #ход сделан успешно, присваиваем единицу
+                count += 1 #ход сделан успешно, присваиваем единицу
             else:
                 print('На этой клетке уже стоит знак, поставьте знак '
                       'на пустую клетку \n')
